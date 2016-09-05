c54a13d0ce64cedd2ee4f4e9586f858db992d8ea
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-05 19:01:55 +1000

    Nodemon checking

---
+++ nodemon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+##
+# What happened
+# $ ssh -p 8022 mitko@localhost who-am-i
+# $ mkdir jenkins
+# $ cd jenkins
+# $ git init
+# $ git add remote origin ssh://mitko@localhost:8022/workflowLibs.git
+# $ git push --set-upstream origin master
+# $ git branch --set-upstream-to=origin/master master
+# $ export PATH=/Development/Tools/nvm-1.1.1/v6.3.1:$PATH
+# $ npm install --save-dev nodemon
+git add --all
+git commit -am "Nodemon checking"
+git push
