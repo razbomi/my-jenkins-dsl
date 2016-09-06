686d58b2a858c860fac07fbf276814e34d6b4ad1
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-06 15:21:45 +1000

    Nodemon checking

--- vars/startBrowserStack.groovy
+++ vars/startBrowserStack.groovy
@@ -21,6 +21,7 @@ def call(user, localIdentifier) {
             -force \
             ${BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1
         #echo $!
+        # & echo \\\$! > ${BS_WORK_SPACE}/browserstack.pid
         #cat ${BS_WORK_SPACE}/browserstack.pid
     '''
   }
