d19dc349293284ee12cbe2c3c01ac0f06b782448
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-06 13:53:15 +1000

    Nodemon checking

--- vars/startBrowserStack.groovy
+++ vars/startBrowserStack.groovy
@@ -1,15 +1,17 @@
 def call(user, localIdentifier) {
-  def end.BS_DOWNLOAD_URL = 'https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip'
+  def env.BS_DOWNLOAD_URL = 'https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip'
   def env.BS_WORK_SPACE = pwd()
+  def env.BS_LOCAL_IDENTIFIER = localIdentifier
 
-  echo "Starting browser stack for ${user} in ${BS_WORK_SPACE}"
+
+  echo "Starting browser stack for ${user} in ${env.BS_WORK_SPACE}"
   withCredentials([[$class          : 'UsernamePasswordMultiBinding',
                     credentialsId   : user,
                     usernameVariable: 'BS_USERNAME',
                     passwordVariable: 'BS_TOKEN'
                    ]]) {
-    sh """
-        curl -sS ${end.BS_DOWNLOAD_URL} > ${BS_WORK_SPACE}/BrowserStackLocal.zip
+    sh '''
+        curl -sS ${BS_DOWNLOAD_URL} > ${BS_WORK_SPACE}/BrowserStackLocal.zip
         unzip -o ${BS_WORK_SPACE}/BrowserStackLocal.zip -d ${BS_WORK_SPACE}
         chmod +x ${BS_WORK_SPACE}/BrowserStackLocal
         nohup ${BS_WORK_SPACE}/BrowserStackLocal -v \
@@ -17,8 +19,8 @@ def call(user, localIdentifier) {
             -localIdentifier ${localIdentifier} \
             -forcelocal \
             -force \
-            ${env.BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1 & echo \\\$! > ${BS_WORK_SPACE}/browserstack.pid
+            ${BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1 & echo \\\$! > ${BS_WORK_SPACE}/browserstack.pid
         echo ${BS_WORK_SPACE}/browserstack.pid
-    """
+    '''
   }
 }
