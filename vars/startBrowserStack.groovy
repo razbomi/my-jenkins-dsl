b69a7f1f36fb60f29b025190c6cfb31a14ec3971
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-06 10:37:49 +1000

    Nodemon checking

--- vars/startBrowserStack.groovy
+++ vars/startBrowserStack.groovy
@@ -13,8 +13,11 @@ def withBs(user) {
                    ]]) {
 
     // TODO: Parametarize this script
+    url = "https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip"
+    folder = 
+
     sh """
-        curl -sS https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip > /var/tmp/BrowserStackLocal.zip
+        curl -sS ${url} > /var/tmp/BrowserStackLocal.zip
         unzip -o /var/tmp/BrowserStackLocal.zip -d /var/tmp
         chmod +x /var/tmp/BrowserStackLocal
         nohup /var/tmp/BrowserStackLocal -onlyAutomate -localIdentifier ${env.JOB_NAME} -forcelocal -force -v ${env.TOKEN} > /var/tmp/browserstack.log 2>&1 & echo \\\$! > /var/tmp/browserstack.pid
