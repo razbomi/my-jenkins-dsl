0e1eedef70b7c5627807d0ac9f50cf9f505b4bc8
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-06 10:05:08 +1000

    Nodemon checking

---
+++ vars/browserStack.groovy
@@ -0,0 +1,22 @@
+def call(user) {
+  def workingDir = pwd()
+
+  echo "Starting browser stack for ${user} in ${workingDir}"
+}
+
+def startBrowserStackLocal(user) {
+  withCredentials([[$class          : 'UsernamePasswordMultiBinding',
+                    credentialsId   : user,
+                    usernameVariable: 'USERNAME',
+                    passwordVariable: 'TOKEN'
+                   ]]) {
+
+    // TODO: Parametarize this script
+    sh """
+        curl -sS https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip > /var/tmp/BrowserStackLocal.zip
+        unzip -o /var/tmp/BrowserStackLocal.zip -d /var/tmp
+        chmod +x /var/tmp/BrowserStackLocal
+        nohup /var/tmp/BrowserStackLocal -onlyAutomate -localIdentifier ${env.JOB_NAME} -forcelocal -force -v ${env.TOKEN} > /var/tmp/browserstack.log 2>&1 & echo \\\$! > /var/tmp/browserstack.pid
+    """
+  }
+}
--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,5 +1,5 @@
 // vars/helloWorld.groovy
 def call(name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
-    echo "Hello gyo, ${name}"
+    echo "Hello go, ${name}"
 }
