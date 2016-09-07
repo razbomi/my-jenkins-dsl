9caa8e85f8d48ffbf59b6267b32caf9c0c5eedf4
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-07 17:24:36 +1000

    Nodemon checking

---
+++ vars/startBrowserStack - Copy.groovy
@@ -0,0 +1,52 @@
+/**
+ * https://www.browserstack.com/local-testing
+ *
+ * darwin-x64
+ * linux-x64
+ * linux-ia32
+ * win32
+ */
+def call(
+  user, 
+  localIdentifier, 
+  installFolder = '.',
+  downloadUrl = 'https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip'
+) {
+
+  echo "Starting browser stack ${localIdentifier} for ${user} in ${installFolder}"
+
+  def variables = [
+    "BS_DOWNLOAD_URL=${downloadUrl}",
+    "BS_WORK_SPACE=${installFolder}",
+    "BS_LOCAL_IDENTIFIER=${localIdentifier}"
+  ]
+
+  def credentials = [
+      $class          : 'UsernamePasswordMultiBinding',
+      credentialsId   : user,
+      usernameVariable: 'BS_USERNAME',
+      passwordVariable: 'BS_TOKEN'
+  ]
+
+  withCredentials([credentials]) { 
+    withEnv(variables) { 
+      installScript() 
+    }
+  }
+}
+
+def installScript() {
+  sh '''
+      curl -sS ${BS_DOWNLOAD_URL} > ${BS_WORK_SPACE}/BrowserStackLocal.zip
+      unzip -o ${BS_WORK_SPACE}/BrowserStackLocal.zip -d ${BS_WORK_SPACE}
+      chmod +x ${BS_WORK_SPACE}/BrowserStackLocal
+      nohup ${BS_WORK_SPACE}/BrowserStackLocal -v \
+          -onlyAutomate \
+          -localIdentifier ${BS_LOCAL_IDENTIFIER} \
+          -forcelocal \
+          -force \
+          ${BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1 &
+      echo $! > ${BS_WORK_SPACE}/browserstack.pid
+      cat ${BS_WORK_SPACE}/browserstack.pid
+  '''
+}
