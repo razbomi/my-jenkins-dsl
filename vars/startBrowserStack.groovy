9d02aa699fb09bfb873f3b2b489e2e33f5b8a8cc
Mitko Razboynikov <mitko.razboynikov@transport.nsw.gov.au>
2016-09-07 17:30:28 +1000

    Nodemon checking

--- vars/startBrowserStack.groovy
+++ vars/startBrowserStack.groovy
@@ -1,52 +1,30 @@
-/**
- * https://www.browserstack.com/local-testing
- *
- * darwin-x64
- * linux-x64
- * linux-ia32
- * win32
- */
-def call(
-  user, 
-  localIdentifier, 
-  installFolder = '.',
-  downloadUrl = 'https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip'
-) {
+def call(browserStackCredentials, localIdentifier, workSpace = '.') {
+  env.BS_DOWNLOAD_URL = 'https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip'
+  env.BS_WORK_SPACE = workSpace 
+  env.BS_LOCAL_IDENTIFIER = localIdentifier
 
-  echo "Starting browser stack ${localIdentifier} for ${user} in ${installFolder}"
-
-  def variables = [
-    "BS_DOWNLOAD_URL=${downloadUrl}",
-    "BS_WORK_SPACE=${installFolder}",
-    "BS_LOCAL_IDENTIFIER=${localIdentifier}"
-  ]
+  echo "Starting browser stack for ${browserStackCredentials} in ${env.BS_WORK_SPACE}"
 
   def credentials = [
       $class          : 'UsernamePasswordMultiBinding',
-      credentialsId   : user,
+      credentialsId   : browserStackCredentials,
       usernameVariable: 'BS_USERNAME',
       passwordVariable: 'BS_TOKEN'
   ]
 
   withCredentials([credentials]) { 
-    withEnv(variables) { 
-      installScript() 
-    }
+    sh '''
+        curl -sS ${BS_DOWNLOAD_URL} > ${BS_WORK_SPACE}/BrowserStackLocal.zip
+        unzip -o ${BS_WORK_SPACE}/BrowserStackLocal.zip -d ${BS_WORK_SPACE}
+        chmod +x ${BS_WORK_SPACE}/BrowserStackLocal
+        nohup ${BS_WORK_SPACE}/BrowserStackLocal -v \
+            -onlyAutomate \
+            -localIdentifier ${BS_LOCAL_IDENTIFIER} \
+            -forcelocal \
+            -force \
+            ${BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1 &
+        echo $! > ${BS_WORK_SPACE}/browserstack.pid
+        cat ${BS_WORK_SPACE}/browserstack.pid
+    '''
   }
-}
-
-def installScript() {
-  sh '''
-      curl -sS ${BS_DOWNLOAD_URL} > ${BS_WORK_SPACE}/BrowserStackLocal.zip
-      unzip -o ${BS_WORK_SPACE}/BrowserStackLocal.zip -d ${BS_WORK_SPACE}
-      chmod +x ${BS_WORK_SPACE}/BrowserStackLocal
-      nohup ${BS_WORK_SPACE}/BrowserStackLocal -v \
-          -onlyAutomate \
-          -localIdentifier ${BS_LOCAL_IDENTIFIER} \
-          -forcelocal \
-          -force \
-          ${BS_TOKEN} > ${BS_WORK_SPACE}/browserstack.log 2>&1 &
-      echo $! > ${BS_WORK_SPACE}/browserstack.pid
-      cat ${BS_WORK_SPACE}/browserstack.pid
-  '''
 }
